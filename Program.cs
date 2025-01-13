b4d7ad157be018ad4c1459ccbbe8eabd6fa638f4
Rakindu Marambe <116582517+RakinduM@users.noreply.github.com>
2025-01-13 15:45:29 +0530

    Add connection string for SQLite database and update Program.cs to use configuration

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Dotnet_Core_Project.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connString = "Data Source=GameStore.db";
+var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
